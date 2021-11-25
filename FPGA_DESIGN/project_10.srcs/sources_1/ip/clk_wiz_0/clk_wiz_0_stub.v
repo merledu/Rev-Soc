f0052f803653032c21f290e448e79f232c651331
RehanEjaz <rejaz@students.uit.edu>
2021-11-25 14:40:45 +0500

    FPGA Design Added

---
+++ FPGA_DESIGN/project_10.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+// Date        : Tue Nov 23 15:37:50 2021
+// Host        : kali running 64-bit Kali GNU/Linux Rolling
+// Command     : write_verilog -force -mode synth_stub
+//               /opt/Xilinx/Vivado/2018.2/bin/project_10/project_10.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35ticsg324-1L
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_wiz_0(clk_out1, resetn, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,resetn,clk_in1" */;
+  output clk_out1;
+  input resetn;
+  input clk_in1;
+endmodule
