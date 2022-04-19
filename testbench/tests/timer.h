24207a8a6c3be7c0d062a50c68d51a51fd30e307
Rehan Ejaz <rejaz@students.uit.edu>
2022-04-19 13:45:11 +0500

    Timer header file added

---
+++ testbench/tests/timer.h
@@ -0,0 +1,13 @@
+#define TIMER_BASE_ADDRESS  0x20002000
+#define CTRL                0x0
+#define CFG                 0x100
+#define TIMER_L             0x104
+#define TIMER_U             0x108
+#define CMP_L               0x10c
+#define CMP_U               0x110
+#define INTR_EN             0x114
+
+void delay(int a);
+
+
+
