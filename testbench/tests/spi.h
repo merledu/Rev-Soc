e5bc821c52023e7f17f20f32e1b2fce68924fc43
Rehan Ejaz <rejaz@students.uit.edu>
2022-04-25 11:56:11 +0500

    Header file addded for SPI

---
+++ testbench/tests/spi.h
@@ -0,0 +1,8 @@
+#define SPI_BASE             0x20006000
+#define CLK_DIV    0x4
+#define LENGTH_REG 0x10
+#define CMD_REG    0x8
+#define ADDR_REG   0xC
+#define DUMMY_CYC  0x14
+#define DATA_REG   0x18
+#define STATUS_REG 0x00
